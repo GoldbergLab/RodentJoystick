45400bbbe06e433ab207c99b8b377c1f522de0c2
Goldberg Lab Administrator <glab.cornell@gmail.com>
2015-03-02 16:13:43 -0500

    adds trajectory analysis scripts -NS

---
+++ PostProcessing/hold_time_distr.m
@@ -0,0 +1,8 @@
+function [ ] = hold_time_distr(stats)
+%hold_time_distr plots the distribution of hold times with a histogram
+%   interval specified by hist_int
+
+tstruct = stats.traj_struct;
+
+end
+
---
+++ PostProcessing/trajectory_analysis.m
@@ -0,0 +1,7 @@
+function [ output_args ] = trajectory_analysis( input_args )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
