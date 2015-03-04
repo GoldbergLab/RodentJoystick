6011f5c3ec0973ecb3fc4787c072f1f7315ba264
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-03-04 15:05:19 -0500

    some progress on trajectory analysis (committing for intermediate testing)

--- PostProcessing/hold_time_distr.m
+++ PostProcessing/hold_time_distr.m
@@ -1,4 +1,4 @@
-function [ht_distr, holdtimes] = hold_time_distr(tstruct, hist_int, plotflag)
+function [holdtimes,ht_distr, phandle] = hold_time_distr(tstruct, hist_int, plotflag)
 TIME_RANGE = 2000;
 %hold_time_distr plots the distribution of hold times with a histogram
 %   interval specified by hist_int on a time range of 0 to TIME_RANGE
@@ -7,6 +7,10 @@ TIME_RANGE = 2000;
 %   tstruct :: structure containing all the information about each
 %       trajectory. NOT the same as jstruct - after running
 %       stats = xy_getstats, tstruct = stats.traj_struct
+%   hist_int :: size of the bins for data generation and histogram plotting
+%   plotflag :: flag that tells hold_time_distr whether to generate data or
+%       plot - 'plot' - plots the figure, 'data' - just returns data
+%       without plotting
 
 holdtimes = zeros(length(tstruct), 1);
 for i = 1:length(tstruct)
@@ -16,7 +20,7 @@ timerange = 0:hist_int:TIME_RANGE;
 [ht_distr] = histc(holdtimes, timerange);
 
 if strcmp(plotflag, 'plot')
-    plot = figure(1);
+    phandle = figure(1);
     xlabel('Hold Time (milliseconds)');
     stairs(timerange, ht_distr, 'b');
 end
--- PostProcessing/trajectory_analysis.m
+++ PostProcessing/trajectory_analysis.m
@@ -1,5 +1,5 @@
-function [ output_args ] = trajectory_analysis(bin_length)
-%UNTITLED2 Summary of this function goes here
+function [sortedtraj ] = trajectory_analysis(stats, bin_length)
+%trajectory_analysis(stats, bin_length)
 %   ARGUMENTS:
 
 %place into bins (groups), mini structures
@@ -8,6 +8,32 @@ function [ output_args ] = trajectory_analysis(bin_length)
     %for each data point
     % don't normalize (all on 100ms time scale) -> (4 above)
 % should be able to plot?
-
+TIME_RANGE = 2000;
+bins=0:bin_length:TIME_RANGE;
+sortedtraj = struct('geq', 'lt', 'trajectories'); 
+% will be a vector of structs containing fields for the range
+% each struct has a field for a vector of structs containing
+% trajectories
+tstruct=stats.traj_struct; 
+holdtimes = hold_time_distr(tstruct, bin_length, 'data');
+for i = 2:length(bins)
+   sortedtraj(i-1) = struct('geq', bins(i-1),'lt',bins(i));
 end
 
+for i = 1:length(holdtimes)
+    bin_ind = bin_index(bins, holdtimes(i));
+end
+
+end
+% Bin indexing starts with 1 at the first nonzero element. Ie, If the bins
+% are distributed as bins = [0 10 20 30 40],
+%       bin_index(bins, 5) = 1, bin_index(bins 0) = 1
+%       bin_index(bins, 9) = 1, bin_index(bins 10) = 2
+
+function bin_ind = bin_index(bins, time)
+    for i = 2:length(bins)
+        if time < bins(i)
+            bin_ind = i-1; break;
+        end
+    end
+end
