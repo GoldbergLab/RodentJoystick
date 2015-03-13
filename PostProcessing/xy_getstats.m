d6713908be63fbac077b24202acbf1d8835cb61a
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-03-13 17:16:42 -0400

    more minor changes

--- PostProcessing/xy_getstats.m
+++ PostProcessing/xy_getstats.m
@@ -72,71 +72,8 @@ for struct_index=1:length(jstruct)
  rw_onset = jstruct(struct_index).reward_onset;
  js_pairs_r = jstruct(struct_index).js_pairs_r;
  js_pairs_l = jstruct(struct_index).js_pairs_l;
- js_reward = jstruct(struct_index).js_reward;
-
- %% Process  
-    
-%  if numel(js_pairs_r)>0 && numel(np_pairs)>0
-%      for j=1:size(js_pairs_r,1)
-%          %check if js movement happens in between nosepokes
-%          if(sum(((np_pairs(:,1)-js_pairs_r(j,1))<0)&((np_pairs(:,2)-js_pairs_r(j,1))>0))>0)
-%              c = (np_pairs(:,1)-js_pairs_r(j,1))<0;
-%              start_p_temp = start_p;
-%              start_p = max(np_pairs(c,1));
-%              np_end = np_pairs((np_pairs(c,1)==start_p),2);
-%              stop_p = min(js_pairs_r(j,2),np_end);
-%              start_i = max((js_pairs_r(j,1)-1000),start_p);
-%              if (numel(start_p)>0)  && ~isequaln(start_p_temp,start_p)
-%                  traj_x_t = traj_x(start_i:stop_p);
-%                  traj_y_t = traj_y(start_i:stop_p);
-%                  traj_x_t1 = traj_x(js_pairs_r(j,1):stop_p);
-%                  traj_y_t1 = traj_y(js_pairs_r(j,1):stop_p);
-%                  % js_post = js_vect_l((js_pairs_r(j,1)):(stop_p))>2;
-%                  
-%                  if ((traj_x(start_p)^2+traj_y(start_p)^2)^(0.5))<50
-%                      mag_traj = ((traj_x_t.^2+traj_y_t.^2).^(0.5));
-%                      mag_traj1 = ((traj_x_t1.^2+traj_y_t1.^2).^(0.5));
-%                      above_thresh=mag_traj>thresh;
-%                      a=find(above_thresh);
-%                      if numel(a)<1
-%                          a = find(mag_traj==max(mag_traj));
-%                      end
-%                      
-%                      k=k+1;
-%                      
-%                      traj_struct(k).traj_x = traj_x_t;
-%                      traj_struct(k).traj_y = traj_y_t;
-%                      % traj_struct(k).js_post = js_post;
-%                      traj_struct(k).start_p = start_p;
-%                      traj_struct(k).stop_p = stop_p;
-%                      traj_struct(k).rw = js_reward(j);
-%                      traj_struct(k).magtraj = mag_traj1;
-%                      traj_struct(k).magatnp = ((traj_x(start_p)^2+traj_y(start_p)^2)^(0.5));
-%                      traj_struct(k).max_value_ind = find(mag_traj==max(mag_traj));
-%                      traj_struct(k).max_value = max(mag_traj);
-%                      traj_struct(k).posttouch = stop_p-js_pairs_r(j,1);
-%                      %traj_struct(k).th_index = a(1);
-% %                      offset = a(1);
-%                      
-%                      
-%                      mX_jsoffset = [traj_y_t' traj_x_t'];
-%                      %                        mX_thindex =  [traj_y_t(1:a(1))' traj_x_t(1:a(1))'];
-%                      vYEdge = -100:2:100;
-%                      vXEdge = -100:2:100;
-%                      if (k>k1 && k<k2)
-%                          if (traj_struct(k).max_value>1)
-%                           traj_pdf_jsoffset = traj_pdf_jsoffset + double(hist2d (mX_jsoffset, vYEdge, vXEdge)>0);
-%                          end
-%                          %                        traj_pdf_thindex = traj_pdf_thindex + hist2d(mX_thindex,vYEdge,vXEdge);
-%                      end
-%                  end
-%                  
-%              end
-%          end
-%      end
-%  end
- 
- %% Process
+ js_reward = jstruct(struct_index).js_reward;  
+ %% Process, and develop traj_struct
  start_temp =0;
  
    if numel(js_pairs_r)>0 && numel(np_pairs)>0 && numel(js_pairs_l)>0
@@ -151,10 +88,13 @@ for struct_index=1:length(jstruct)
                     np_js_temp = (np_pairs(:,1)-js_pairs_r(j,1))<0; 
                     %set of nose poke onsets preceding the js onset
                     start_p = max(np_pairs(np_js_temp,1));
-                    %Nose poke before the Joystick touch
+                    %Nose poke before the Joystick touch is the most recent
+                    %touch 
                     np_end = np_pairs((np_pairs(np_js_temp,1)==start_p),2); 
                     %corresponding nose poke offset
                     
+                    %count as new trial only if prev joystick attempt
+                    %wasn't on the same nosepoke onset offset
                     if (start_p ~= start_temp)                   
                      trialnum = trialnum+1;
                     end
