5308bcec19aaca41c2733b4ac7d0297d781557f6
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-03-28 15:36:02 -0400

    some more reorganization of files

---
+++ PostProcessing/TrajectoryAnalysis/trajectory_location_distr.m
@@ -0,0 +1,7 @@
+function [ output_args ] = trajectory_location_distr( input_args )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
--- PostProcessing/doallstatsinfolder.m
+++ PostProcessing/doallstatsinfolder.m
@@ -3,7 +3,7 @@ function stats = doallstatsinfolder(wdir)
 list = dirrec(wdir,'jstruct*');
 for i=1:length(list)
     load(list{i});
-    stats(i) = xy_getstats(jstruct,[0 100000]);
+    stats(i) = xy_getstats(jstruct);
     clear jstruct;
 end
 
