90a36ba241a39629786b36f35d3437b5601a4346
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-03-28 15:11:56 -0400

    edits trajectory analysis: plots median and related statistics on default, though still supports mean plotting. documentation also updated

---
+++ PostProcessing/TrajectoryAnalysis/trajectory_analysis.m
@@ -0,0 +1,192 @@
+%[sortedtraj, fh] = 
+%   trajectory_analysis(stats) OR trajectory_analysis(stats, plot_range, ... )
+%   OPTIONAL ARG ORDER:
+%       plot_range,hold_time_range,pflag, plot_contingencies, datestr,sflag
+%   plots the trajectory distributions from stats using the (optional)
+%   arguments for a hold time range, whether or not to plot, whether to
+%   plot the lines indicating the contigencies (constants just below the
+%   function header), and the string representation of the date
+%   It also returns fh, the figure handle, if plotted - otherwise fh is not
+%   assigned
+%   EXAMPLE:  
+%       trajectory_analysis(stats,4)
+%       trajectory_analysis(stats,4,[1000 1600], [400 40 40])
+%   OUTPUTS:
+%       sortedtraj :: a struct with the following fields:
+%
+%       fh :: the figure handle corresponding to the plot generate when
+%           'plot' flag is used, otherwise unassigned
+%   ARGUMENTS: 
+%       stats :: the result from xy_getstats(jstruct) for some jstruct
+%       OPTIONAL ARGS:
+%       plot_range :: number representing number of plots. DEFAULT: 10
+%       hold_time_range :: the time range [A B] (ms) for which plots are
+%           generated. Plots start at time A and end at B
+%           DEFAULT: [400 1400]
+%       plot_contingencies :: [HT T1 T2] this vector tells which lines to
+%           plot - HT is the hold time, T1 and T2 are the respective
+%           deviations (i.e. 30 60)
+%           value of [0 0] or [0 0 0] doesn't plot any lines
+%           DEFAULT: [300 30 60] - no plotting 
+%       pflag :: 'plot' if plot is desired, otherwise just returns
+%           binned trajectories in sortedtraj
+%           DEFAULT: 'plot'
+%       datestr :: string representation of the date, used for title
+%           DEFAULT: 'N/A'
+%       sflag :: optional argument that tells what group of statistics to
+%           plot. 'median' - plots median and first/third quartiles,
+%           'mean'- plots mean and mean+/- stdev, 'both' - plots both
+%           groups
+%           DEFAULT: 'N/A'
+function [sortedtraj, fh] = trajectory_analysis(stats, varargin)
+%how many plots to do - this depends on other stuff, look through
+%below as well. Look through subplotting routine to make sure nothing
+%critical is changed
+
+default = {10,[400 1400], [300 30 60], 'plot', 'N/A', 'median'};
+numvarargs = length(varargin);
+if numvarargs > 6
+    error('trajectory_analysis: too many arguments (> 7), only one required and six optional.');
+end
+[default{1:numvarargs}] = varargin{:};
+[PLOT_RANGE,TIME_RANGE, CONTL, pflag, datestr, sflag] = default{:};
+
+%divide the desired time range into number of bins based on number of plots
+%desired
+bin_length = (TIME_RANGE(2) - TIME_RANGE(1))/PLOT_RANGE;
+bins=TIME_RANGE(1):bin_length:TIME_RANGE(2);
+tstruct=stats.traj_struct; 
+totaltraj = length(tstruct);
+holdtimes = hold_time_distr(tstruct, bin_length, 'data');
+sortedtraj = sort_traj_into_bins(tstruct, bins, holdtimes);
+
+%sometimes we only want the data from sorted traj, hence the option not to
+%plot
+if strcmp('plot', pflag)
+    fh = figure('Position', [100, 100, 1440, 900]);
+    for i = 1:PLOT_RANGE
+        bin = sortedtraj(i);
+        [mean, median, stdev, numbers, upperbnd, lowerbnd] = bin_stats(bin);
+        time = 1:1:length(numbers);
+        inittraj = numbers(1); normalized = 100*numbers./inittraj;
+        tstr = strcat(num2str(bin.geq), '-', num2str(bin.lt), ' ms:');
+        percent = num2str(100*inittraj/totaltraj, 4); format bank;
+        titlestr = [tstr, num2str(inittraj), ' trajectories, ', percent,' %'];
+        
+        subplot(2, PLOT_RANGE/2, i);
+        axis([0, bin.lt, 0, 100]);
+        title(titlestr); hold on;
+        
+        if strcmp(sflag, 'median')
+        plot(time, upperbnd, 'r', time, median, 'g', time, lowerbnd, 'r'); hold on;
+        elseif strcmp(sflag, 'mean')
+        plot(time, mean+stdev, 'y', time, mean, 'b', time, mean-stdev, 'y'); hold on;
+        else
+        plot(time, upperbnd, 'r', time, median, 'g', time, lowerbnd, 'r'); hold on;
+        plot(time, mean+stdev, 'y', time, mean, 'b', time, mean-stdev, 'y'); hold on;
+        end
+        
+        plot(time, normalized, ':c');
+        if sum(CONTL)>0
+            CONTLINE_COLORS = [0.4, 0.4, 0.4];
+            line([0 2000], [CONTL(2) CONTL(2)], 'Color', CONTLINE_COLORS);
+            if length(CONTL)>2
+                line([0 2000], [CONTL(3) CONTL(3)], 'Color', CONTLINE_COLORS);
+            end
+            line([CONTL(1) CONTL(1)], [0 100], 'Color', CONTLINE_COLORS);
+        end
+        ylabel('Joystick Mag/Traj Percentage');
+        xlabel('Time(ms)')
+    end
+    if strcmp(sflag, 'median')
+    legend('3rd quartile','median', '1st quartile');
+    elseif strcmp(sflag, 'mean')
+    legend('mean+stdev','mean', 'mean-stdev');
+    else
+    legend('3rd quartile','median', '1st quartile', 'mean+stdev','mean', 'mean-stdev');
+    end
+    P = subplottitle(fh, datestr, 'yoff', -0.6);
+end
+end
+
+%bin_summary is a struct with length bin.lt 
+%   bin_summary has the following fields (for each time i)(different from the outputs!!!): 
+%       position := a vector of magnitudes corresponding to the various trajectories at time i.
+%       avg := single value corresponding to the average position at that
+%           time
+%       med := single value of median position at that time
+%       stdev := a single value that corresponds to the standard deviation
+%           at the time i
+%       numtraj := number of trajectories that lasted at least as long as
+%       time i.
+% mean, median, std are all numbers representing the statistic in a given
+% time. upperbnd, lowerbnd are the 75th and 25th percentiles, respectively
+function [avg, med, stdev, numbers, upperbnd, lowerbnd, bin_summary] = bin_stats(bin)
+    for time = 1:(bin.lt-1) 
+        time_pos_ind = 0;
+        for i = 1:(length(bin.trajectory))
+            try 
+                pos = bin.trajectory(i).magtraj(time); 
+                %attempt to access the position of trajectory i at time
+            catch
+                pos = -1000; % error signal if trajectory doesn't go that far
+            end
+            if pos > -1
+                time_pos_ind = time_pos_ind+1;
+                bin_summary(time).position(time_pos_ind) = pos;
+            end
+        end
+        try
+            positionvec = bin_summary(time).position;
+            bin_summary(time).numtraj = time_pos_ind;
+            bin_summary(time).avg = mean(positionvec);
+            bin_summary(time).med = median(positionvec);
+            bin_summary(time).stdev = std(positionvec);
+            bin_summary(time).upperbnd = prctile(positionvec,75);
+            bin_summary(time).lowerbnd = prctile(positionvec,25);
+            
+            numbers(time)= bin_summary(time).numtraj;
+            avg(time) = bin_summary(time).avg;
+            med(time) = bin_summary(time).med;
+            stdev(time) = bin_summary(time).stdev;
+            upperbnd(time) = bin_summary(time).upperbnd;
+            lowerbnd(time) = bin_summary(time).lowerbnd;
+        catch
+            avg(time) = 0; med(time) = 0; stdev(time) = 0; numbers(time) = 0; upperbnd(time)=0;lowerbnd(time)=0; 
+        end
+    end
+end
+
+% will be a vector of structs containing fields for the range
+% each struct has a field for a vector of structs containing
+% trajectories
+function sortedtraj = sort_traj_into_bins(tstruct, bins, holdtimes)
+
+for i = 2:length(bins)
+   sortedtraj(i-1) = struct('geq', bins(i-1),'lt',bins(i));
+end
+bin_traj_indices = ones(length(bins)-1, 1); 
+%each bin has a vector of trajectory structures (simplified from tstruct)
+% store the indices so we know at what index to add each new trajectory
+for i = 1:length(holdtimes)
+    bin_ind = bin_index(bins, holdtimes(i));
+    if bin_ind ~= -1
+        traj_ind = bin_traj_indices(bin_ind);
+        bin_traj_indices(bin_ind) = bin_traj_indices(bin_ind) + 1;
+        sortedtraj(bin_ind).trajectory(traj_ind)= struct('magtraj', tstruct(i).magtraj, 'time', holdtimes(i));
+    end
+end
+end
+
+% Bin indexing starts with 1 at the first nonzero element. Ie, If the bins
+% are distributed as bins = [0 10 20 30 40],
+%       bin_index(bins, 5) = 1, bin_index(bins 0) = 1
+%       bin_index(bins, 9) = 1, bin_index(bins 10) = 2
+function bin_ind = bin_index(bins, time)
+    bin_ind = -1;
+    for i = 2:length(bins)
+        if time < bins(i) && time >= bins(i-1)
+            bin_ind = i-1; break;
+        end
+    end
+end
---
+++ PostProcessing/xy_makestruct.m
@@ -0,0 +1,121 @@
+% xy_makestruct(working_dir) takes all the .mat files in working_dir and
+% processes all of them to create a single jstruct that stores a summary of
+% all the data from a single day:
+% the resulting jstruct is a vector where each entry is a structure with
+% numerous fields.
+% So each jstruct(i) for all i has the following fields
+%   filename :: name of the combined .mat file in the following format: 
+%       [frameno (10)]_Box_[0-9]_[Date (9)] - number in parentheses
+%       indicates number of characters
+%   path :: another structure which is a vector listing the .dat files used
+%       to generate the combined .mat file. Since this usually is not
+%       necessary, further information can be found by looking at the
+%       structure itself
+%   real_time :: the time (in MATLAB standard double representation of
+%   time) of the beginning of the element in the jstruct.
+%       (so subsequent absolute time is with real_time + onset/1000) where
+%       onset is from data below
+%
+%   NOTE: all following information uses milliseconds as a time scale with
+%   time 0 being the start of the entire block of data.
+%
+%   traj_x :: vector containing the x-trajectory information
+%   traj_y :: vector containing the y-trajectory information
+%   np_pairs :: an m x 2 array containing the pairs of (onset, offset)
+%       times of the nose poke sensor. m>0
+%   js_pairs_r :: an m x 2 array containing the pairs of (onset, offset)
+%       times of the joystick touch sensor. m>=0
+%   js_pairs_r :: an m x 2 array containing the pairs of (onset, offset)
+%       times of the post touch sensor. m>=0
+function [jstruct] = xy_makestruct(working_dir)
+
+working_dir_1 = strcat(working_dir,'/','comb');
+filelist = dir(strcat(working_dir_1,'/*.mat'));
+load(strcat(working_dir_1,'\',filelist(1).name));
+
+fileinfo = dir(strcat(working_dir, '\', filelist(1).name(1:end-4),'.dat'));
+time_stamp = fileinfo.datenum;
+% start_frame = str2num(filelist(1).name(1:end-4));
+start_frame_first = start_frame;
+
+for i=1:length(filelist)         
+    load(strcat(working_dir_1,'/',filelist(i).name));
+    working_buff=working_buff';
+    
+    % nose pokes 
+    np = sensor_on_off_times(working_buff(5,:));   
+    % first touch_sense
+    js_r = sensor_on_off_times(working_buff(3,:));   
+    % second touch sense
+    js_l = sensor_on_off_times(working_buff(4,:));   
+    %mark reward times
+    reward_del = working_buff(6,:);
+    reward_logical = (reward_del>0.5);
+    reward_on = find(diff(reward_logical)==1);
+    js_reward = zeros(1,size(js_r,1));
+   
+  % Mark rewarded joystick deflections
+    for j=1:length(reward_on)
+        if numel(js_r>0)
+            a = find(((reward_on(j)-js_r(:,2))<0)&((reward_on(j)-js_r(:,1))>0));
+            if numel(a)>0
+                js_reward(a) = 1;
+            else
+                diff_a = (reward_on(j)-js_r(:,2));
+                diff_a = diff_a(diff_a>0);
+                a = (diff_a==min(diff_a));
+                js_reward(a) = 1;
+            end
+        end
+    end
+    jstruct(i).filename = filelist(i).name;
+    jstruct(i).path = dir;
+    jstruct(i).traj_x = working_buff(1,:);
+    jstruct(i).traj_y = working_buff(2,:);
+    jstruct(i).np_pairs = np;
+    jstruct(i).js_pairs_r = js_r;
+    jstruct(i).js_pairs_l = js_l;
+    jstruct(i).reward_onset = reward_on;
+    jstruct(i).js_reward = js_reward;
+    jstruct(i).real_time = time_stamp+(start_frame-start_frame_first)*(1/(24*60*60));
+
+      % Mark nose pokes prior to start of each joystick deflection
+    start_p = zeros(size(jstruct(i).js_pairs_r,1),1);
+    if numel(jstruct(i).js_pairs_r)>0 && numel(jstruct(i).np_pairs)>0
+        for j=1:size(jstruct(i).js_pairs_r,1)
+            if(sum(((np(:,1)-js_r(j,1))<0)&((np(:,2)-js_r(j,1))>0))>0)            
+                temp = (np(:,1)-js_r(j,1))<0;
+                start_p(j) = max(np(temp));            
+            end 
+        end
+    end
+   
+   jstruct(i).np_js_start = start_p;
+end
+end
+
+% pairs] = sensor_on_off_times(rawsens) generates a list of sensor 
+% onset/offset pairs for example, pairs(1, 1) and pairs(1,2) will return 
+% the times that a nosepoke came on and went off for the first nosepoke
+% in the file
+function [pairs] = sensor_on_off_times(rawsens)
+    pairs = [];
+    sens_logic = (rawsens>0.5);
+    l=1;
+    if sum(sens_logic)>1
+        sense_transition = diff(sens_logic); %this will show the transitions 
+        b=sense_transition;
+        flag=1;       
+        for j=1:length(b)        
+            if b(j)==1 && flag==1
+                pairs(l,1) = j+1;
+                pairs(l,2) = 0;
+                flag=0;
+            elseif b(j)==-1 && flag==0        
+                pairs(l,2) = j-1;
+                flag=1;
+                l=l+1;
+            end  
+        end
+    end
+end
