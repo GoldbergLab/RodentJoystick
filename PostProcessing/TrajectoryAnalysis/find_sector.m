766686faa69a6dc1549c0ec3593b6226e2f6de43
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-03-30 00:42:53 -0400

    changes to find_sector, all raw data processing is there, should be able to implement union algorithm now

--- PostProcessing/TrajectoryAnalysis/find_sector.m
+++ PostProcessing/TrajectoryAnalysis/find_sector.m
@@ -1,21 +1,63 @@
-function [angle_distr] = find_sector(stats, thresh, colorperc, pflag)
+function [angle_distr, fh, trajindices] = find_sector(stats, varargin)
+%argument handling
+default = {95, 'log', [20 80]};
+numvarargs = length(varargin);
+if numvarargs > 3
+    error('trajectory_analysis: too many arguments (> 3), only one required and three optional.');
+end
+[default{1:numvarargs}] = varargin{:};
+[thresh, pflag, colorperc] = default{:};
+%if strcmp(pflag, 'log'); colorperc = [0 95]; end;
 
 tstruct = stats.traj_struct;
 %360th slot corresponds to 0
-angle_distr = zeros(360,1);
+%trajectory_indices:
+trajindices(1) = struct('traj_ind', []);
+for i= 2:360; trajindices(i) = struct('traj_ind', []); end;
+angle_distr = zeros(360,1); sample_size = 0;
 for i = 1:length(tstruct)
     [first, second] = get_sector(tstruct(i).traj_x,tstruct(i).traj_y,thresh);
     if first ~= -1 && second ~= -1
         if (first == 0); first = 360; end
         if (second == 0); second = 360; end
-        indices = first:second;
-        if (first>second); indices = [first:360, 1:second]; end
+        if (first>second)
+            indices = [first:360, 1:second];
+            %add information to struct containing trajectory in
+            for j = first:360
+                trajindices(j).traj_ind = [trajindices(j).traj_ind; i];
+            end
+            for j = 1:second
+                trajindices(j).traj_ind = [trajindices(j).traj_ind; i];
+            end 
+        else
+            indices = first:second;
+            for j = indices
+                trajindices(j).traj_ind = [trajindices(j).traj_ind; i];
+            end
+        end
+        %add information about sector covered by trajectory to 
+        %angle_distribution 
         angle_distr(indices)=1+angle_distr(indices);
+        %note that not all trajectories are actually used, in computing a
+        %quarter, we only end up taking the subset above the trajectory
+        sample_size = sample_size + 1;
     end
 end
+for i = 1:360; trajindices(i).traj_ind = sort(trajindices(i).traj_ind); end
+fh = draw_plots(stats, angle_distr, pflag, colorperc);
+if sample_size < 10
+    vec = ['Threshhold is too large, or not enough data. Not enough samples.'];
+    error(vec);
+end
 
-%Plot results from traj_pdf
-subplot(1,3,1);
+end
+
+function [first, second] = calc_target_sector(start_angle, traj_indices)
+    
+end
+
+function fh = draw_plots(stats, angle_distr, pflag, colorperc)
+%just gathering data, with some processing;
 data = stats.traj_pdf_jstrial;
 if strcmp(pflag, 'log')
     data = log(data);
@@ -25,26 +67,44 @@ else
     traj_pdf = reshape(data, 100*100, 1);
     traj_pdf = sort(traj_pdf(traj_pdf ~= 0));
 end
+
+fh = figure('Position', [100, 100, 1200, 500]);
+%Plot results from traj_pdf
+subplot(1,3,1); hold on;
+title(['Trajectory Distribution: (',pflag,' scale)']); 
+xlabel('X Position+50'); ylabel('Y Position + 50');
 pcolorval2 = traj_pdf(floor(colorperc(2)/100*length(traj_pdf)));
 pcolorval1 = traj_pdf(floor(colorperc(1)/100*length(traj_pdf))+1);
-pcolor(data); shading flat; axis square; caxis([pcolorval1 pcolorval2]); hold on;
-%Plot angle histogram
+pcolor(data); shading flat; axis square; caxis([pcolorval1 pcolorval2]); hold off;
 
-subplot(1,3,2); %plot actual angle distribution 
-axis([1 359 0 inf]); hold on
+%grey color values for linear angle distribution
 colorv = [0.75 0.7 0.6 0.5];
+
+%plot normalized angle distribution
+subplot(1,3,2); 
+axis([1 359 0 inf]); axis square; hold on;
 for i = 25:25:100
     angle_dist = get_angle_distr_for_thresh(stats, i);
     c = colorv(i/25);
-    plot(1:1:360, angle_distr, 'Color', [c c c]);
+    plot(1:1:360, angle_dist./(sum(angle_dist)), 'Color', [c c c]);
 end
-plot(1:1:360, angle_distr, 'r');
+plot(1:1:360, angle_distr./sum(angle_distr), 'r');
 
+%plot polar angle distribution
 subplot(1,3,3);
 theta = (1:1:360)*pi./180;
-polar(theta', angle_distr);
-
+axmax = max(angle_distr./sum(angle_distr));
+for i = 25:25:100
+    angle_dist = get_angle_distr_for_thresh(stats, i); c = colorv(i/25); hold on;
+    l=polar(theta', angle_dist./sum(angle_dist)); hold on;
+    axmax = max([axmax, max(angle_dist./sum(angle_dist))]);
+    set(l,'Color', [c c c]);
 end
+polar(theta', angle_distr./sum(angle_distr), 'b'); hold on;
+axis([(-axmax) axmax (-axmax) axmax]); axis square;
+hold off;
+end
+
 
 function [angle_distr] = get_angle_distr_for_thresh(stats, thresh)
 tstruct = stats.traj_struct;
