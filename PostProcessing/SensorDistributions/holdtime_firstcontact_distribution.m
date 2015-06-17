ceddbcf4aab0a1bfdd898c31428663effec5bb24
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-06-17 11:23:34 -0400

    put try/catch in holdtime_first contact

--- PostProcessing/SensorDistributions/holdtime_firstcontact_distribution.m
+++ PostProcessing/SensorDistributions/holdtime_firstcontact_distribution.m
@@ -5,11 +5,16 @@ function [ hold_dist ] = holdtime_firstcontact_distribution(jstruct, varargin)
 [~,hold_dist]=xy_holddist(jstruct,dist_thresh_ip,0.75);    
 dist_time_hld = 0:10:600;
 holddist_vect = histc(hold_dist,dist_time_hld);
-figure(3)
-xlabel('Time (ms)'); ylabel('# of trials');
-title('JS first contact hold time at thresh');
-stairs(dist_time_hld, holddist_vect,'b','LineWidth',2);
-hold on
+
+try
+    axes(ax);
+    xlabel('Time (ms)'); ylabel('# of trials');
+    title('JS first contact hold time at thresh');
+    stairs(dist_time_hld, holddist_vect,'b','LineWidth',2);
+    hold on
+catch
+    error('Failure in plotting');
+end
 
 end
 
