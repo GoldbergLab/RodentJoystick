b60da25ef253e745ce2e6ce036c02183475b69eb
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-07-06 16:56:44 -0400

    New automation files

---
+++ Automation/scheduled_analysis.m
@@ -0,0 +1,10 @@
+function  = scheduled_analysis( input_args )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+%put the main directory containing experiment data here.
+toprocesslist = directories_to_do('K:DataSync');
+multi
+
+end
+
