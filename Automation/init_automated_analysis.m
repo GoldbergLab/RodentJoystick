50e50fc90cc008766f290e056812c6b60882f23a
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-07-07 17:43:19 -0400

    automated with timing finished - also now contains behavior reports

--- Automation/init_automated_analysis.m
+++ Automation/init_automated_analysis.m
@@ -1,10 +1,17 @@
-%calling this function sets up the automated analysis timer - cleans
+%calling this script sets up the automated analysis timer - cleans
 %workspace after it's done.
 
-period = 5; %minutes
+start_time = [17 20]; %time is currently set to 1AM (24 hour time)
+period = 24*60; %minutes
 
-automated_analysis_timer = timer('StartDelay', 30, 'Period', 60*period, 'ExecutionMode', 'fixedRate');
+time = now;
+desired_seconds = start_time(1)*60*60 + start_time(2)*60;
+actual_seconds = (time - floor(time))*24*60*60;
+delay = desired_seconds-actual_seconds;
+if delay<0; delay = delay+24*60*60; end;
+automated_analysis_timer = timer('StartDelay', delay, 'Period', 60*period, 'ExecutionMode', 'fixedRate');
 automated_analysis_timer.StartFcn = @(~,~)disp(['Beginning automated analysis every ', num2str(period),' minutes.']);
 automated_analysis_timer.TimerFcn = @(~,~)scheduled_analysis;
 start(automated_analysis_timer);
 clear period;
+clear start_time; clear desired_seconds; clear actual_seconds; clear delay; clear time;
--- Automation/scheduled_analysis.m
+++ Automation/scheduled_analysis.m
@@ -10,7 +10,9 @@ recipients={'nitin.shyamkumar@gmail.com', ...
             };%add new recipients of daily reports here
 %log directory
 logsdirloc = 'J:\Users\GLab\Documents\RodentProjectAutomatedLogs';
-
+%normal pellet count - anything outside of this range is specificalyl
+%highlighted in the mail notification
+normal_pellets = [110 300];
 
 time = now;
 toprocesslist = directories_to_do(experiment_directory);
@@ -18,24 +20,30 @@ title = {'Analysis attempted on the following directories:','', ''};
 %attempt all analysis here
 [failure, actual, succeed] = multi_doAll(toprocesslist, 2);
 pp_report = [title; failure];
+[bhvr_summary, bhvr_report] = behavior_report(toprocesslist, normal_pellets(1), normal_pellets(2));
 
 try
-title = ['Analysis_', datestr(time,'mm_dd_yyyy_HH_MM')];
-logname = [logsdirloc,'\',title,'.txt'];
-fileID = fopen(logname,'w');
-for i = 1:size(pp_report, 1)
-    formatspec = '%s\r\n %s\r\n\r\n';
-    fprintf(fileID, formatspec, pp_report{i, [1, 3]});
-end
-fclose(fileID);
+    title = ['Analysis_', datestr(time,'mm_dd_yyyy_HH_MM')];
+    logname = [logsdirloc,'\',title,'.txt'];
+    fileID = fopen(logname,'w');
+    for i = 1:size(pp_report, 1)
+        formatspec = '%s\r\n %s\r\n\r\n';
+        fprintf(fileID, formatspec, pp_report{i, [1, 3]});
+    end
+    for i = 1:size(bhvr_report, 1)
+        formatspec = '%s %s %s\r\n';
+        fprintf(fileID, formatspec, bhvr_report{i, :});
+    end
+    fclose(fileID);
 catch
     disp('failed log write');
 end
 pp_summary = ['Attempted processing on ', num2str(actual),...
                 ' directories, succeeded on ', num2str(succeed),' of them.', ...
                 ' Full report is attached.'];
+summary = [pp_summary, bhvr_summary];
 try
-    matlabmail(recipients, pp_summary, title, logname);
+    matlabmail(recipients, summary, title, logname);
 catch
     disp('failed mail log');
 end
