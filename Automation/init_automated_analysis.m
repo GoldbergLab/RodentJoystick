0e2dd0e5ba7c0e3b68803dad245361137717ece4
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-07-07 15:45:49 -0400

    mostly local changes I believe (meaning directory changes), but should be fairly final now. only thing left to do is behavior reports as far as I'm aware.

--- Automation/init_automated_analysis.m
+++ Automation/init_automated_analysis.m
@@ -1,7 +1,7 @@
 %calling this function sets up the automated analysis timer - cleans
 %workspace after it's done.
 
-period = 0.75; %minutes
+period = 5; %minutes
 
 automated_analysis_timer = timer('StartDelay', 30, 'Period', 60*period, 'ExecutionMode', 'fixedRate');
 automated_analysis_timer.StartFcn = @(~,~)disp(['Beginning automated analysis every ', num2str(period),' minutes.']);
