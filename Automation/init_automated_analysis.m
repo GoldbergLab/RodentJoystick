7456c109df78bc50b7e15440748b7e9dce95bf4e
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-07-08 09:51:41 -0400

    updates to automation scripts

--- Automation/init_automated_analysis.m
+++ Automation/init_automated_analysis.m
@@ -1,17 +1,22 @@
 %calling this script sets up the automated analysis timer - cleans
 %workspace after it's done.
 
-start_time = [17 20]; %time is currently set to 1AM (24 hour time)
+%% MODIFY THESE PARAMETERS TO SCHEDULE ANALYSIS
+start_time = [09 49]; %time is currently set to 9AM (24 hour time)
 period = 24*60; %minutes
+expt_dir = 'K:\automationtest\0009';
+logs_dir = 'J:\Users\GLab\Documents\RodentProjectAutomatedLogs';
 
+%% UNNECESSARY TO CHANGE ON A REGULAR BASIS
 time = now;
 desired_seconds = start_time(1)*60*60 + start_time(2)*60;
 actual_seconds = (time - floor(time))*24*60*60;
-delay = desired_seconds-actual_seconds;
+delay = round(desired_seconds-actual_seconds);
 if delay<0; delay = delay+24*60*60; end;
 automated_analysis_timer = timer('StartDelay', delay, 'Period', 60*period, 'ExecutionMode', 'fixedRate');
 automated_analysis_timer.StartFcn = @(~,~)disp(['Beginning automated analysis every ', num2str(period),' minutes.']);
-automated_analysis_timer.TimerFcn = @(~,~)scheduled_analysis;
+automated_analysis_timer.TimerFcn = @(~,~)scheduled_analysis(expt_dir, logs_dir);
+automated_analysis_timer.StopFcn = @(~,~)disp('Automated analysis stopped.');
 start(automated_analysis_timer);
 clear period;
 clear start_time; clear desired_seconds; clear actual_seconds; clear delay; clear time;
