255245c10f0a9a2eee16d81b821b1e0b4399eafd
Goldberg Lab Administrator <glab.cornell@gmail.com>
2015-08-03 13:38:50 -0400

    some new files added for anlys_gui and new variability analysis functions

---
+++ xy_anlys_gui/plot_raw_data.m
@@ -0,0 +1,7 @@
+function [ output_args ] = plot_raw_data(handles, axnum, np, js, post, x, y, dev)
+%plot_raw_data plots raw sensor information onto axes axnum. np, js, post,
+%x, y, dev, are all 1/0 flags instructing whether or not to plot raw data.
+
+
+end
+
