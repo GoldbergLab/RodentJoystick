e74b9508fa95967a41210a40a64282f2ad65b5ae
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-08-04 17:21:51 -0400

    updates sort_traj_into_bin, adding features for selecting definition of hold time, and option to filter rewarded trajectories - also adds more detailed documentation for the function

--- PostProcessing/PPUtilities/sort_traj_into_bins.m
+++ PostProcessing/PPUtilities/sort_traj_into_bins.m
@@ -1,21 +1,79 @@
-% sortedtraj is a struct that stores a group of trajectories in the field
-% called trajectory
-%   sortedtraj(i) contains all trajectories with holdtimes in the range
-%   [sortedtraj(i).geq, sortedtraj(i).lt)
-%   there is also a field trajectory, which is another struct containing
-%       multiple trajectories' information - can be velocity, acceleration,
-%       pos.
-function sortedtraj = sort_traj_into_bins(tstruct, bins)
+function sortedtraj = sort_traj_into_bins(tstruct, bins, varargin)
+% sortedtraj = sort_traj_into_bins(tstruct, bins ,[rwfilter, ht_definition]) 
+%
+%   generates a struct representation of a trajectory struct binned by hold
+%   time. Also supports filtering by reward (returns only rewarded
+%   trajectories).
+%
+% OUTPUTS:
+%
+%   sortedtraj :: struct with length equal to length(bins)-1. 
+%       Each cell of sortedtraj (sortedtraj(i)) has the following fields
+%
+%           qeq :: greater than or equal to - the lower hold time limit 
+%               (inclusive) as a double of all trajectories in the cell
+%
+%           lt :: less than - the upper hold time limit for all
+%               trajectories (also a double) in the cell
+%
+%           traj_struct :: a trajectory structure with the exact same
+%               fields as tstruct (from the stats structure). Possibly
+%               empty if bins & rwfilter are poorly selected.
+%
+% ARGUMENTS
+%
+%   tstruct :: the trajectory structure to be binned (hold time
+%       from the field rw_or_stop, not length)
+%
+%   bins :: a vector defining how the trajectories should be binned: 
+%       EX: bins = [200 300 400 500] will result in sortedtraj containing 3
+%       bins, defined by the ranges 200-300, 300-400, 400-500.
+%
+% OPTIONAL ARGS
+%
+%   rwfilter :: a flag indicating whether or not to filter by reward - if
+%       on (1), then only trajectories in tstruct that have been rewarded
+%       will be sorted and added to the bins. Otherwise (0) bins all
+%       trajectories in tstruct
+%       DEFAULT - 0
+%
+%   ht_definition :: flag indicating which definition of hold time should
+%       be used for binning;
+%       0 - rw_or_stop
+%       1 - length(traj_x)
+%       DEFAULT - 0
+
+%% Argument Manipulation
+default = {0, 0};
+numvarargs = length(varargin);
+if numvarargs > 1
+    error('too many arguments (> 3), only two required and one optional.');
+end
+[default{1:numvarargs}] = varargin{:};
+[rwfilter, ht_definition] = default{:};
+
+%% Create struct representation based on bins, and indices to keep track of
+% where trajectory should go
 for i = 2:length(bins)
    sortedtraj(i-1) = struct('geq', bins(i-1),'lt',bins(i));
 end
 bin_traj_indices = ones(length(bins)-1, 1); 
+
+%% Iterate over each trajectory in tstruct, binning appropriately
 %each bin has a vector of trajectory structures (simplified from tstruct)
 % store the indices so we know at what index to add each new trajectory
 for i = 1:length(tstruct)
     %This is where our definition of hold time comes into effect:
-    bin_ind = bin_index(bins,tstruct(i).rw_or_stop);
-    if bin_ind ~= -1
+    if ht_definition
+        ht = length(tstruct(i).traj_x);
+    else
+        ht = tstruct(i).rw_or_stop;
+    end
+    bin_ind = bin_index(bins,ht);
+    
+    %first check if it was binned in the range, if it was 
+    % check that it was rewarded if rwfilter was on
+    if bin_ind ~= -1 && (tstruct(i).rw || ~rwfilter)
         traj_ind = bin_traj_indices(bin_ind);
         bin_traj_indices(bin_ind) = bin_traj_indices(bin_ind) + 1;
         sortedtraj(bin_ind).trajectory(traj_ind)= tstruct(i);
