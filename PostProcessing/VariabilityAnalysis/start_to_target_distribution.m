33a83e561e6d18415e57b71d6a48f64b1a174ec4
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-08-04 16:49:42 -0400

    adds beginning of variability analysis toolkit

---
+++ PostProcessing/VariabilityAnalysis/start_to_target_distribution.m
@@ -0,0 +1,11 @@
+function [ output_args ] = start_to_target_distribution(stats, varargin)
+% start_to_target_distribution(stats, [ht_range, rw_filter, axlst])
+%
+%   generates plots of the distribution of distances from a trajectory's
+%   start to its end point. start_to_target_distribution takes a stats 
+%   structure (typically representing an entire contingency's data) and 
+%   bins by hold time.
+
+
+end
+
