b7ea12c4babdd39a1c6edc935a1cb0f2b329167a
Nitin Shyamkumar <nitinshyamk@gmail.com>
2015-08-07 17:19:02 -0400

    changes to both draw_heat_map and trajectory_variability_heat_map

--- PostProcessing/VariabilityAnalysis/trajectory_variability_heat_map.m
+++ PostProcessing/VariabilityAnalysis/trajectory_variability_heat_map.m
@@ -1,4 +1,4 @@
-function [ output_args ] = trajectory_variability_heat_map(stats, varargin)
+function [ output_args ] = trajectory_variability_heat_map(statslist, varargin)
 %[ output_args ] = trajectory_variability_heat_map(stats, [numgroups, min_ht, axlst])
 %
 %   trajectory_variability_heat_map analyzes the inherent variability in
@@ -10,8 +10,9 @@ function [ output_args ] = trajectory_variability_heat_map(stats, varargin)
 %
 % ARGUMENTS:
 %
-%   stats :: standard stats structure (typically across an entire
-%       contingency to evaluate variability and learning
+%   stats :: list of stats structure with field traj_struct
+%       (typically across an entire contingency to evaluate 
+%           variability and learning)
 %
 % OPTIONAL ARGS:
 %
@@ -19,7 +20,7 @@ function [ output_args ] = trajectory_variability_heat_map(stats, varargin)
 %       analysis
 %       DEFAULT - 3
 %
-%   comparediff :: plot differences in the distribution (1), rather than 
+%   normalize :: plot differences in the distribution (1), rather than 
 %       the distributions themselves (0)
 %       
 %   ht_range :: only trajectories in the range ht_range will be considered
@@ -46,62 +47,88 @@ if numvarargs > 7
     error('too many arguments (> 8), only 1 required and 7 optional.');
 end
 [default{1:numvarargs}] = varargin{:};
-[numgroups, comparediff, ht_range, threshold, bin_size, rew_filter, axlst] = default{:};
-comparediff = ~~comparediff; %make sure 1/0;
-if length(axlst)<(numgroups-comparediff)
+[ht_range, threshold, bin_size, rew_filter, axlst] = default{:};
+numgroups = length(statslist);
+if length(axlst)<numgroups
     figure('Position', [200 200 400 800]);
-    for i = 1:(numgroups-comparediff);
-        axlst(i) = subplot(numgroups, 1, i);
+    axlst(1) = subplot(numgroups, 2, 1); 
+    for i = 2:numgroups;
+        axlst(2*i-1) = subplot(numgroups, 2, 2*i-1);        
+        axlst(2*i) = subplot(numgroups, 2, 2*i);
     end
 end
 
-tstruct = stats.traj_struct;
-bin = sort_traj_into_bins(tstruct, ht_range, rew_filter, 1); 
-tstruct = bin.traj_struct;
+angle_bins = -180:bin_size(1):180;
+rad_bins = threshold:bin_size(2):100;
 
-indices = floor(linspace(1, numgroups+1-2*eps(numgroups), length(tstruct)));
-endpoints = [1, find(diff(indices)), length(tstruct)];
-for i = 1:(length(endpoints)-1)
-    data = generate_heat_map_data(tstruct(endpoints(i):endpoints(i+1)), ...
-        threshold, bin_size);
+for i = 1:numgroups
+    tstruct = statslist(i).traj_struct;
+    bin = sort_traj_into_bins(tstruct, ht_range, rew_filter, 1); 
+    tstruct = bin.traj_struct;
     
-    angle_bins = -180:bin_size:180;
-    rad_bins = threshold:bin_size:100;
+    [mu, sigma, data3d] = ...
+        generate_heat_map_data( tstruct, ...
+        threshold, angle_bins, rad_bins);
     
-    if ~comparediff
-        draw_heat_map(data, axlst(i), 'Trajectory Variability', 1, [5 95], ...
-            angle_bins(1:end-1), rad_bins(1:end-1));
-    else
-        try
-            distrdiff = olddata - data;
-            draw_heat_map(distrdiff, axlst(i-1), 'Trajectory Variability Difference', ...
-                0, [1 99], angle_bins(1:end-1), rad_bins(1:end-1));
-        catch e
-            disp(getReport(e));
-        end
+    draw_heat_map(mu, axlst(2*i-1), 'Trajectory Variability', 0, [5 95], ...
+        angle_bins(1:end-1), rad_bins(1:end-1));
+    colorbar;
+    
+    try
+        avg_zscore = get_zscore(oldmu, oldsigma, data3d);
+        draw_heat_map(avg_zscore, axlst(2*i), ...
+            'Trajectory Variability Difference', ...
+            0, [1 99], angle_bins(1:end-1), rad_bins(1:end-1));
+        colorbar;
+    catch e
+        disp(getReport(e));
     end
-    olddata = data;
+    oldmu = mu; oldsigma = sigma;
 end
 
 end
 
 %only adds points above a certain range to the heat map
-function data = generate_heat_map_data(tstruct, threshold, interv)
-    angle_bins = -180:interv:180;
-    rad_bins = threshold:interv:100;
-    accumulator = zeros(length(rad_bins)-1, length(angle_bins)-1);
+function [mu, sigma, data3d] = ...
+    generate_heat_map_data(tstruct, threshold, angle_bins, rad_bins)
+
+    data3d = zeros(length(rad_bins)-1, length(angle_bins)-1, ...
+        length(tstruct));
     for i = 1:length(tstruct)
         tx = tstruct(i).traj_x;
         ty = tstruct(i).traj_y;
-        [rad, theta] = align_target(tx, ty, 1);
+        [rad, theta] = map_trajectory(tx, ty, 0);
         theta = theta(rad>threshold);
         rad = rad(rad>threshold);
-        accumulator = accumulator + hist2d([rad' theta'], rad_bins, angle_bins);
+        normalize = 0;
+        traj_prob = hist2d([rad' theta'], rad_bins, angle_bins);
+        traj_prob = ~~traj_prob; %binary histogram - visit or not?
+        if normalize && sum(sum(traj_prob))>0
+            traj_prob = traj_prob./sum(sum(traj_prob));
+        end 
+        data3d(:, :, i) = traj_prob;
     end
-    data = accumulator/length(tstruct);
+    mu = sum(data3d, 3)/length(tstruct);
+    sigma = std(data3d, 0, 3);
 end
 
-function [rad, theta] = align_target(traj_x, traj_y, target_select)
+function [avg_zscore] = get_zscore(mu_1, sigma_1, data3d_2)
+    for i = 1:size(data3d_2)
+        traj = data3d_2(:, :, i);
+        traj = traj-mu_1;
+        
+        %avoid NaNs
+        traj(sigma_1 == 0) = 0;
+        sigma_1(sigma_1 == 0) = 1;
+        
+        zscore = traj./sigma_1;
+        data3d_2(:, :, i) = zscore;
+    end
+    avg_zscore = sum(data3d_2, 3)./size(data3d_2, 3);
+
+end
+
+function [rad, theta] = map_trajectory(traj_x, traj_y, target_select)
 % performs coordinate transformation  of traj_x and traj_y to polar
 % coordinate system and then aligns by polar angle of end point.
 
@@ -109,15 +136,14 @@ function [rad, theta] = align_target(traj_x, traj_y, target_select)
 theta = theta*180/pi;
 
 %align by angle to radial line of end point.
-if target_select
+if target_select==1
     [~, maxind] = max(rad);
     theta = theta - theta(maxind);
-else
+elseif target_select == 2
     theta = theta - theta(end);
 end
 
 %now make sure we put everything in the range -180 to 180
 theta = theta-360*(theta>180)+360*(theta<-180);
-
 
 end
